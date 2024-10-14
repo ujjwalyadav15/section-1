f6c03a7fce7d7bf50897c149927c1c4dd7d89008
ujjwalyaduvanshi2@gmail.com <ujjwalyaduvanshi2@gmail.com>
2024-10-14 17:21:26 +0530

    condition

---
+++ javascript/condition.js
@@ -0,0 +1,28 @@
+num = 52;
+
+if(num%2 ==0){
+ console.log('even');
+}else{
+    console.log('odd');
+    
+}
+
+// WAP to check if person is eligible for DL
+
+age=18
+if(age>=18){
+
+    abc = 'something';
+    let a= 'local';
+    const pi = 3.14;
+    // pi=3.14
+    
+    console.log('not eligible');
+    
+}else{
+    console.log('eligible');
+    
+}
+console.log(abc);
+//console.log(a);
+
--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,25 @@
 console.log("Hello JavaScript");
+
+a=234;
+
+console.log(a);
+
+myname = 'ujjwal yadav';
+
+console.log(myname);
+
+console.log( typeof myname);
+console.log(typeof a);
+
+price = 89.65;
+
+console.log(typeof price);
+
+console.log( 1==1);
+console.log('1'==1);//check values
+console.log('1'===1);//check value + type
+
+
+
+
+
