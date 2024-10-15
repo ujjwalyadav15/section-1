20f41ecaf7a3f6ad8ba9d4e7f51cc9ccd83f4902
ujjwalyaduvanshi2@gmail.com <ujjwalyaduvanshi2@gmail.com>
2024-10-15 14:54:09 +0530

    homework

---
+++ javascript/homework.js
@@ -0,0 +1,36 @@
+//WAP to print grade on the basis of marks
+marks =75
+if (90<=marks) {
+   console.log('Grade=A+');   
+}else if (80<=marks) {
+    console.log('Grade=A'); 
+}else if (70<=marks) {
+    console.log('Grade=B');
+}
+else if (60<=marks) {
+    console.log('Grade=C');
+}else if (50<=marks) {
+    console.log('Grade=D');  
+}else if (40<=marks) {
+    console.log('grade=E');
+    
+}else{
+    console.log('Fail');
+    
+}
+
+//WAP to check if number is a perfect square.
+n = 4
+
+
+// WAP to check if a number is divisible by both 7 and 11.
+
+n = 77
+if (n % 7==0 && n % 11==0) {
+    console.log('This number is divisible by 7 & 11');
+    
+}else {
+    console.log('This number is not divisible by 7 & 11');
+    
+    
+}
