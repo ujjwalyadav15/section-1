6b74f4cffd9cfd450a7ed1faa4fbc44c7b709e13
ujjwalyaduvanshi2@gmail.com <ujjwalyaduvanshi2@gmail.com>
2024-10-16 21:41:38 +0530

    hw

---
+++ javascript/homework2.js
@@ -0,0 +1,12 @@
+//WAP to print Fibonacci series.
+let n1 = 0, n2 = 1, nextTerm;
+
+console.log('Fibonacci Series:');
+
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}
+// WAP to reverse a number.
