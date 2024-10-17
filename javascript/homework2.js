490978142ed858c60d5981285846beb4a6ad9abb
ujjwalyaduvanshi2@gmail.com <ujjwalyaduvanshi2@gmail.com>
2024-10-17 15:13:57 +0530

    .

--- javascript/homework2.js
+++ javascript/homework2.js
@@ -3,10 +3,22 @@ let n1 = 0, n2 = 1, nextTerm;
 
 console.log('Fibonacci Series:');
 
-for (let i = 1; i <= number; i++) {
+for (let i = 0; i <= 10; i++) {
     console.log(n1);
     nextTerm = n1 + n2;
     n1 = n2;
     n2 = nextTerm;
 }
 // WAP to reverse a number.
+
+let num = 57439;
+let reverse = 0;
+
+while (num > 0) {
+    console.log(num, reverse);
+
+    let d = num % 10;
+    reverse = reverse * 10 + d;
+    num = parseInt(num / 10);
+}
+console.log(reverse);
